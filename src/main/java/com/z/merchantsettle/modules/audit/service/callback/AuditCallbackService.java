95b1957dac6e86a9dacdb15d37de5480b55ec9d0
zincpool <zincpool@gmail.com>
2019-05-09 23:38:49 +0800

    upd:abstract map

--- src/main/java/com/z/merchantsettle/modules/audit/service/callback/AuditCallbackService.java
+++ src/main/java/com/z/merchantsettle/modules/audit/service/callback/AuditCallbackService.java
@@ -1,49 +1,35 @@
 package com.z.merchantsettle.modules.audit.service.callback;
 
+import com.google.common.collect.Maps;
 import com.z.merchantsettle.exception.AuditException;
 import com.z.merchantsettle.modules.audit.constants.AuditConstant;
 import com.z.merchantsettle.modules.audit.constants.AuditTypeEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class AuditCallbackService {
 
-    @Autowired
-    private  CustomerCallbackHandler customerCallbackHandler;
-    @Autowired
-    private  CustomerKpCallbackHandler customerKpCallbackHandler;
-    @Autowired
-    private  CustomerContractCallbackHandler customerContractCallbackHandler;
-    @Autowired
-    private  CustomerSettleCallbackHandler customerSettleCallbackHandler;
-    @Autowired
-    private  WmPoiBaseInfoCallbackHandler wmPoiBaseInfoCallbackHandler;
-    @Autowired
-    private  WmPoiQuaCallbackHandler wmPoiQuaCallbackHandler;
-    @Autowired
-    private  WmPoiDeliveryInfoCallbackHandler wmPoiDeliveryInfoCallbackHandler;
-    @Autowired
-    private  WmPoiBusinessInfoCallbackHandler wmPoiBusinessInfoCallbackHandler;
+    private static Map<Integer, AbstractHandler> handlerMap = Maps.newConcurrentMap();
+
+    static {
+        handlerMap.put(AuditTypeEnum.CUSTOMER.getCode(), new CustomerCallbackHandler());
+        handlerMap.put(AuditTypeEnum.CUSTOMER_KP.getCode(), new CustomerKpCallbackHandler());
+        handlerMap.put(AuditTypeEnum.CUSTOMER_CONTRACT.getCode(), new CustomerContractCallbackHandler());
+        handlerMap.put(AuditTypeEnum.CUSTOMER_SETTLE.getCode(), new CustomerSettleCallbackHandler());
+        handlerMap.put(AuditTypeEnum.POI_BASE_INFO.getCode(), new WmPoiBaseInfoCallbackHandler());
+        handlerMap.put(AuditTypeEnum.POI_QUA_INFO.getCode(), new WmPoiQuaCallbackHandler());
+        handlerMap.put(AuditTypeEnum.POI_DELIVERY_INFO.getCode(), new WmPoiDeliveryInfoCallbackHandler());
+        handlerMap.put(AuditTypeEnum.POI_BUSINESS_INFO.getCode(), new WmPoiBusinessInfoCallbackHandler());
+    }
 
     public  AbstractHandler getCallbackHandler(int auditType) {
 
-        if (AuditTypeEnum.CUSTOMER.getCode() == auditType) {
-            return customerCallbackHandler;
-        } else if (AuditTypeEnum.CUSTOMER_KP.getCode() == auditType) {
-            return customerKpCallbackHandler;
-        } else if (AuditTypeEnum.CUSTOMER_CONTRACT.getCode() == auditType) {
-            return customerContractCallbackHandler;
-        } else if (AuditTypeEnum.CUSTOMER_SETTLE.getCode() == auditType) {
-            return customerSettleCallbackHandler;
-        } else if (AuditTypeEnum.POI_BASE_INFO.getCode() == auditType) {
-            return wmPoiBaseInfoCallbackHandler;
-        } else if (AuditTypeEnum.POI_QUA_INFO.getCode() == auditType) {
-            return wmPoiQuaCallbackHandler;
-        } else if (AuditTypeEnum.POI_DELIVERY_INFO.getCode() == auditType) {
-            return wmPoiDeliveryInfoCallbackHandler;
-        } else if (AuditTypeEnum.POI_BUSINESS_INFO.getCode() == auditType) {
-            return wmPoiBusinessInfoCallbackHandler;
+        AbstractHandler handler = handlerMap.get(auditType);
+        if (handler != null) {
+            return handler;
         }
 
         throw new AuditException(AuditConstant.AUDIT_OP_ERROR, "不存在此审核类型");
